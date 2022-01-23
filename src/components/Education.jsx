42284c87e6df5cbd240a33f6a29631d2f5091b50
jasmine3014 <jasminetripti5@gmail.com>
2022-01-23 18:21:42 +0600

    reverse education information

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -8,21 +8,9 @@ function Education() {
                 <div className="box-container">
                     <div className="box">
                         <i className="fas fa-graduation-cap"></i>
-                        <span>2014</span>
-                        <h3>Secondary School Certificate </h3>
-                        <p>Faridpur Govt Girl`s High School</p>
-                    </div>
-                    <div className="box">
-                        <i className="fas fa-graduation-cap"></i>
-                        <span>2016</span>
-                        <h3>Higher Secondary Certificate</h3>
-                        <p>Faridpur Govt Sarada Sundari Mohila College </p>
-                    </div>
-                    <div className="box">
-                        <i className="fas fa-graduation-cap"></i>
-                        <span>2020</span>
-                        <h3>Trainee - Programming Foundations with JavaScript, HTML and CSS</h3>
-                        <p>Duke University, Durham, NC 27708, United State.</p>
+                        <span>2021</span>
+                        <h3>Bachelor of Science (BSc)</h3>
+                        <p>Bangladesh University of Business and Technology </p>
                     </div>
                     <div className="box">
                         <i className="fas fa-graduation-cap"></i>
@@ -32,15 +20,27 @@ function Education() {
                     </div>
                     <div className="box">
                         <i className="fas fa-graduation-cap"></i>
-                        <span>2021</span>
-                        <h3>Bachelor of Science (BSc)</h3>
-                        <p>Bangladesh University of Business and Technology </p>
+                        <span>2020</span>
+                        <h3>Trainee - Programming Foundations with JavaScript, HTML and CSS</h3>
+                        <p>Duke University, Durham, NC 27708, United State.</p>
                     </div>
                     <div className="box">
                         <i className="fas fa-graduation-cap"></i>
+                        <span>2016</span>
+                        <h3>Higher Secondary Certificate</h3>
+                        <p>Faridpur Govt Sarada Sundari Mohila College </p>
+                    </div>
+                    <div className="box">
+                        <i className="fas fa-graduation-cap"></i>
+                        <span>2014</span>
+                        <h3>Secondary School Certificate </h3>
+                        <p>Faridpur Govt Girl`s High School</p>
+                    </div>
+                    <div className="box">
+                        {/* <i className="fas fa-graduation-cap"></i>
                         <span>2021</span>
                         <h3>Part Time - Frontend Developer</h3>
-                        <p>Devsoftlab - Mirpur, Dhaka-1216, Bangladesh.</p>
+                        <p>Devsoftlab - Mirpur, Dhaka-1216, Bangladesh.</p> */}
                     </div>
                 </div>
             </section>
