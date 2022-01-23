6233270a0f658349d7f03d405858cd9e0b31cfd4
jasmine3014 <jasminetripti5@gmail.com>
2022-01-23 18:20:52 +0600

    employment section add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Education from "./components/Education";
+import Employment from "./components/Employment";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Portfolio from "./components/Portfolio";
@@ -12,6 +13,7 @@ function App() {
             <Home />
             <About />
             <Education />
+            <Employment />
             <Portfolio />
             <Contact />
         </div>    
---
+++ src/components/Employment.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Employment() {
+    return (
+        <div>
+            <section className="employment" id="employment">
+                <h1 className="heading"> my <span>employment</span></h1>
+                <div className="box-container">
+                    <div className="box">
+                        <i className="fas fa-briefcase"></i>
+                        <span>2022</span>
+                        <h3>Web Developer (November 1,2021 – Present)</h3>
+                        <p>Bd Soft  It Solutions - Mirpur DOHS , Dhaka-1216, Bangladesh.</p>
+                    </div>
+                    <div className="box">
+                        <i className="fas fa-briefcase"></i>
+                        <span>2021</span>
+                        <h3>Intern - Software Developer (August 1, 2021– October 30 ,2021)</h3>
+                        <p>Priyojon - Mirpur-01, Dhaka-1216, Bangladesh.</p>
+                    </div>
+                    <div className="box"></div>
+                    <div className="box"></div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Employment
